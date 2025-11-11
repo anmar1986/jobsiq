e6350259c0b41dbec574676145fcf94817ac923c
Anmar <anmar.bahram@gmail.com>
2025-11-12 00:02:13 +0100

    fix merge conflict

--- src/views/dashboard/ProfileView.vue
+++ src/views/dashboard/ProfileView.vue
@@ -7,7 +7,18 @@
           <h1 class="text-3xl font-bold text-gray-900 mb-2">My Profile</h1>
           <p class="text-gray-600">{{ isEditMode ? 'Update your personal information' : 'View your profile information' }}</p>
         </div>
-        <div v-if="!isEditMode">
+        <div v-if="!isEditMode" class="flex gap-3">
+          <BaseButton
+            variant="outline"
+            @click="$router.push({ name: 'change-password' })"
+          >
+            <template #icon-left>
+              <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 7a2 2 0 012 2m4 0a6 6 0 01-7.743 5.743L11 17H9v2H7v2H4a1 1 0 01-1-1v-2.586a1 1 0 01.293-.707l5.964-5.964A6 6 0 1121 9z" />
+              </svg>
+            </template>
+            Change Password
+          </BaseButton>
           <BaseButton
             variant="primary"
             @click="enterEditMode"
