ee4df50c86c2fa182748200d32f8a7a7fc4b5e3c
Anmar <anmar.bahram@gmail.com>
2025-11-18 11:13:04 +0100

    remove unwnated cmponent

--- src/components/base/LoadingSpinner.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="flex items-center justify-center p-4">
-    <div class="animate-spin rounded-full h-8 w-8 border-b-2 border-primary-600"></div>
-  </div>
-</template>
-
-<script setup lang="ts">
-// Simple loading spinner component
-</script>
